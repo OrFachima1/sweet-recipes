6fe46191a3d5cd6409914e17c2552445afc91f27
OrFachima <ah118112@gmail.com>
2025-10-25 11:19:29 +0300

    fix screen upload123

--- src/components/orders/modals/UploadModal.tsx
+++ src/components/orders/modals/UploadModal.tsx
@@ -206,6 +206,7 @@ export default function UploadModal({
 
   const handleSubmit = async () => {
     await onRunPreview(dateOverrides);
+    onClose();
   };
 
   const hasMissingDates = previewOrders.some((order, idx) => {
